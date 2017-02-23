028ed4fec46f8fd175874c580176a4181d5812ad
jk1dd <portajonk@gmail.com>
2017-02-22 18:21:49 -0700

    change to item repo

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,10 +1,24 @@
 class MerchantRepository
+  attr_reader :all
   def initialize(path)
-    @path = path
+    # @path = path
+    @all = []
+    csv_loader(path)
+    merchant_maker
   end
 
   def find_by_name(name)
-    
+
+  end
+
+  def csv_loader(path)
+    @csv = CSV.open(path, headers:true, header_converters: :symbol)
+  end
+
+  def merchant_maker
+    @all = @csv.map do |row|
+      Merchant.new(row, self)
+    end
   end
 
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -16,4 +16,12 @@ class MerchantRepositoryTest < Minitest::Test
   def test_it_can_find_merchant_by_name
     assert_instance_of Merchant, @mr.find_by_name("CJsDecor")
   end
+
+  def test_it_can_create_instance_of_merchant
+    assert_instance_of Merchant
+  end
+
+  def test_it_can_return_array_of_all_known_merchant_instances
+    assert
+  end
 end
